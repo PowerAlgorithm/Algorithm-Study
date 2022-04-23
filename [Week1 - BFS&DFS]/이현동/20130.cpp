0b770926c0918e09b9cf07d975cc2bb0f1c52fbe
LEEHYUNDONG <l.steve10@yahoo.com>
2022-04-23 13:49:58 +0900

    Metroidvania Extreme

--- [Week1 - BFS&DFS]/이현동/20130.cpp
+++ [Week1 - BFS&DFS]/이현동/20130.cpp
@@ -6,7 +6,7 @@ int dx[4] = {1, -1, 0, 0};
 int dy[4] = {0, 0, 1, -1};
 int startX, startY;
 string graph[201];
-int visited[201][201];
+bool visited[201][201];
 bool alphaVisited[26] = {false};
 vector<pair<int, int> > ans;
 
@@ -22,8 +22,8 @@ void bfs(int x, int y)
     deque<pair<int, int> > q; //탐색하는 위치 정보
     map<char, queue<pair<int, int> > > doorq;
     q.push_back(make_pair(x, y));
-    fill(&visited[0][0], &visited[N][M], -1);
-    visited[x][y] = 0;
+    fill(&visited[0][0], &visited[N][M], false);
+    visited[x][y] = true;
     ans.push_back(make_pair(x, y));
 
     while (!q.empty())
@@ -31,9 +31,6 @@ void bfs(int x, int y)
         int x = q.front().first;
         int y = q.front().second;
         q.pop_front();
-        if (find(ans.begin(), ans.end(), make_pair(x, y)) == ans.end())
-            ans.push_back(make_pair(x, y));
-
         for (int i = 0; i < 4; i++)
         {
             int rx = x + dx[i];
@@ -41,45 +38,47 @@ void bfs(int x, int y)
 
             if (checkRange(rx, ry))
             {
-                if (graph[rx][ry] == '#')
-                    continue;
-                if (visited[rx][ry] == -1)
+                if (graph[rx][ry] == '!')
                 {
+                    ans.push_back(make_pair(rx, ry));
+                    return;
+                }
+                if (graph[rx][ry] == '#' || visited[rx][ry])
+                    continue;
 
-                    if (graph[rx][ry] == '!')
+                // 자물쇠가 있는 경우, 자물쇠 큐에 넣어둔다. 즉, 열쇠를 획득하면 바로 문을 doorq에 넣어둔다.
+                //대문자
+                if (('A' <= graph[rx][ry]) && ('Z' >= graph[rx][ry]))
+                {
+                    int tmp = graph[rx][ry] - 'A';
+                    if (!alphaVisited[tmp])
                     {
-                        ans.push_back(make_pair(rx, ry));
-                        return;
+                        doorq[tmp].push(make_pair(rx, ry));
+                        continue;
                     }
-                    // 자물쇠가 있는 경우, 자물쇠 큐에 넣어둔다. 즉, 열쇠를 획득하면 바로 문을 doorq에 넣어둔다.
-                    //대문자
-                    if (('A' <= graph[rx][ry]) && ('Z' >= graph[rx][ry]))
+                }
+                visited[rx][ry] = true;
+                if (find(ans.begin(), ans.end(), make_pair(rx, ry)) == ans.end())
+                    ans.push_back(make_pair(rx, ry));
+                q.push_back(make_pair(rx, ry));
+                //열쇠 아이템이 있는 경우, 열쇠 획득
+                if (('a' <= graph[rx][ry]) && ('z' >= graph[rx][ry]))
+                {
+                    int tmp = graph[rx][ry] - 'a';
+                    alphaVisited[tmp] = true;
+                    while (!doorq[tmp].empty())
                     {
-                        int tmp = graph[rx][ry] - 'A';
-                        if (!alphaVisited[tmp])
+                        int nx, ny;
+                        nx = doorq[tmp].front().first;
+                        ny = doorq[tmp].front().second;
+                        doorq[tmp].pop();
+                        if (!visited[nx][ny])
                         {
-                            doorq[tmp].push(make_pair(rx, ry));
-                            continue;
-                        }
-                    }
-                    visited[rx][ry] = 1;
-                    //열쇠 아이템이 있는 경우, 열쇠 획득
-                    if (('a' <= graph[rx][ry]) && ('z' >= graph[rx][ry]))
-                    {
-                        int tmp = graph[rx][ry] - 'a';
-                        alphaVisited[tmp] = true;
-                        while (!doorq[tmp].empty())
-                        {
-                            int nx, ny;
-                            nx = doorq[tmp].front().first;
-                            ny = doorq[tmp].front().second;
-                            doorq[tmp].pop();
+                            visited[nx][ny] = true;
+                            ans.push_back({nx, ny});
                             q.push_back(make_pair(nx, ny));
                         }
                     }
-                    if (find(ans.begin(), ans.end(), make_pair(rx, ry)) == ans.end())
-                        ans.push_back(make_pair(rx, ry));
-                    q.push_back(make_pair(rx, ry));
                 }
             }
         }
@@ -113,20 +112,20 @@ int main()
     return 0;
 }
 
-// 7 7
-// #######
-// #a***!#
-// #*E*FG#
-// #*D***#
-// #*C***#
-// #1B***#
-// #######
+// // 7 7
+// // #######
+// // #a***!#
+// // #*E*FG#
+// // #*D***#
+// // #*C***#
+// // #1B***#
+// // #######
 
-// 6, 2
-// 5, 2
-// 4, 2
-// 3, 2
-// 2, 2 a
-// 2, 3
-// 2, 4
-// 2, 5
+// // 6, 2
+// // 5, 2
+// // 4, 2
+// // 3, 2
+// // 2, 2 a
+// // 2, 3
+// // 2, 4
+// // 2, 5
