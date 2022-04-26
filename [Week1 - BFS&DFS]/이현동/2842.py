22a7ae231cb066884528f7f65880d80d9718ee5b
LEEHYUNDONG <l.steve10@yahoo.com>
2022-04-26 12:11:55 +0900

    집배원 한상덕

--- [Week1 - BFS&DFS]/이현동/2842.py
+++ [Week1 - BFS&DFS]/이현동/2842.py
@@ -9,63 +9,69 @@ def checkRange(rx, ry):
     return False
 
 
-def bfs(x, y):
-    q = deque([])
-    visited[x][y] = fatigue[x][y]
-    q.append([x, y])
-    res = []
+def bfs(q, x, y, lo, hi):
     p = 0
+    visited = [[False]*N for _ in range(N)]
+    visited[x][y] = True
+
     while q:
         x, y = q.popleft()
-        tmp = deque([])
-        minDiff = int(1e9)  # 최소 피로도 저장
-        flag = False
         for i in range(8):
             rx, ry = x + dx[i], y + dy[i]
             if checkRange(rx, ry):
-                if graph[rx][ry] == 'K' and not visited[rx][ry]:
-                    flag = True
+                if visited[rx][ry]:
+                    continue
+                if (lo <= ff[rx][ry] <= hi):
+                    visited[rx][ry] = True
                     q.append([rx, ry])
-                    visited[rx][ry] = fatigue[rx][ry]
-                    p += 1  # 피로도 증가
-                    if p == post:
-                        return
-                elif graph[rx][ry] == '.':
-                    tmp.append([rx, ry])
-                    minDiff = min(minDiff, abs(
-                        fatigue[rx][ry] - fatigue[x][y]))
-        while tmp and not flag:
-            tx, ty = tmp.popleft()
-            if abs(fatigue[tx][ty] - fatigue[x][y]) == minDiff:
-                q.append([tx, ty])
-                visited[tx][ty] = fatigue[tx][ty]
+                    if graph[rx][ry] == 'K':
+                        p += 1  # 피로도 증가
+    return p
 
 
 N = int(input())
 graph = []  # 그래프
-visited = [[0]*N for _ in range(N)]
 dx = [0, 0, -1, 1, -1, 1, -1, 1]
 dy = [-1, 1, -1, 1, 0, 0, 1, -1]
 start = []
-post = 0
+poffice = []
+fatigue = []  # 피로도
+ff = []
+
 
 for i in range(N):
-    graph.append(list(input().strip()))
+    tmp = list(input().strip())
+    graph.append(tmp)
     for j in range(N):
-        if graph[i][j] == 'P':
+        if tmp[j] == 'P':
             start = [i, j]
-        elif graph[i][j] == 'K':
-            post += 1
+        elif tmp[j] == 'K':
+            poffice.append([i, j])
 
-fatigue = [list(map(int, input().split())) for _ in range(N)]  # 피로도
-
-bfs(start[0], start[1])
-
-res = []
 for i in range(N):
-    print(visited[i])
-    for j in range(N):
-        if visited[i][j] != 0:
-            res.append(visited[i][j])
+    ftmp = list(map(int, input().split()))
+    ff.append(ftmp)
+    fatigue.extend(ftmp)
 
-print(max(res)-min(res))
+fatigue = sorted(set(fatigue))
+l, r = 0, 0
+ans = sys.maxsize
+
+
+while l < len(fatigue):
+    q = deque([])
+    t = 0
+    if fatigue[l] <= ff[start[0]][start[-1]] <= fatigue[r]:
+        q.append([start[0], start[-1]])
+        
+    t = bfs(q, start[0], start[1], fatigue[l], fatigue[r])
+    if t == len(poffice):
+        ans = min(abs(fatigue[l]-fatigue[r]), ans)        
+        l += 1
+    elif r + 1 < len(fatigue):
+        r += 1
+    else:
+        break
+
+
+print(ans)
