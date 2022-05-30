07e7ca7b705018b36019f6a617e85607e922a0aa
LEEHYUNDONG <l.steve10@yahoo.com>
2022-05-30 14:53:34 +0900

    서버실

--- [Week6 - BS&PS]/이현동/17245.cpp
+++ [Week6 - BS&PS]/이현동/17245.cpp
@@ -1,32 +1,36 @@
-#include <cstdio>
-#include <vector>
-#define ull unsigned long long
+#include <bits/stdc++.h>
 using namespace std;
+typedef unsigned long long ull;
 
-int n;
+int N;
 int server[1001][1001];
 
 int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 	ull left = 0, right = 0, sum = 0;
-
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)for (int j = 0; j < n; j++) {
-		scanf("%d", &server[i][j]);
-		sum += server[i][j];
-		if (server[i][j] > right)right = server[i][j];
-	}
+	cin >> N;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+		{
+			cin >> server[i][j];
+			sum += server[i][j];
+			if (server[i][j] > right)
+				right = server[i][j];
+		}
 
 	while (left + 1 < right) {
 		ull mid = (left + right) / 2;
 		ull cnt = 0;
 
-		for (int i = 0; i < n; i++)for (int j = 0; j < n; j++) {
-			cnt += mid > server[i][j] ? server[i][j]:mid;
-		}
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++) {
+				cnt += mid > server[i][j] ? server[i][j]:mid;
+			}
 		if (cnt>=0.5*sum)
 			right=mid;
 		else left = mid;
 	}
-	printf("%lld", right);
+	cout << right;
 	return 0;
 }
